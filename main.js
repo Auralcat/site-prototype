600afaf1428b28d55cba187f547c0fb918c5da41
Auralcat <ashleyzt@hi2.in>
2018-01-30 19:58:35 -0200

    Criar main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  
+  // Expandir texto nas colunas, demonstração para coluna 1
+  $('#texto-expandido1').toggle();
+  $('#col1').hover(function() {
+    $('#texto-pequeno1').toggle("150");
+    $('#texto-expandido1').toggle("150");
+  });
+  
+});
