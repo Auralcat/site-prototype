2befc38f71bde84546e59d857851295c8c798bd0
Auralcat <ashleyzt@hi2.in>
2018-01-30 20:27:54 -0200

    Inverter ordem de importação dos scripts

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   // Expandir texto nas colunas, demonstração para coluna 1
   $('#texto-expandido1').toggle();
   $('#col1').hover(function() {
-    alert("Workin'");
     $('#texto-pequeno1').toggle("150");
     $('#texto-expandido1').toggle("150");
   });
