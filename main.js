bcf8d50a459a43e52b71e1064788d331a94d1d57
Auralcat <ashleyzt@hi2.in>
2018-01-30 20:20:39 -0200

    Arrumando importação

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  
+
   // Expandir texto nas colunas, demonstração para coluna 1
   $('#texto-expandido1').toggle();
   $('#col1').hover(function() {
+    alert("Workin'");
     $('#texto-pequeno1').toggle("150");
     $('#texto-expandido1').toggle("150");
   });
-  
+
 });
