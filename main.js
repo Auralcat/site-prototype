eb18c7b5db710bb126e24ddb35c12de7484522d4
Auralcat <ashleyzt@hi2.in>
2018-01-31 11:35:39 -0200

    Testar expansão coluna 2

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ $(document).ready(function() {
 
   // Expandir texto nas colunas, demonstração para coluna 1
   $('#texto-expandido1').toggle();
+  $('#texto-expandido2').toggle();
+
   $('#col1').hover(function() {
     $('#texto-pequeno1').toggle("150");
     $('#texto-expandido1').toggle("150");
+  });
+
+  // Testar coluna 2
+  $('#col2').hover(function() {
+    $('#texto-pequeno2').toggle("250");
+    $('#texto-expandido2').toggle("250");
   });
 
 });
