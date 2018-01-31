c483727cf2cb631d118541ab98f40e938347f7f2
Auralcat <ashleyzt@hi2.in>
2018-01-31 16:42:41 -0200

    Expandir cada coluna separadamente

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
 $(document).ready(function() {
+    // Esconder o texto maior ao carregar página
+    $('.texto-expandido').hide();
 
-  // Expandir texto nas colunas, demonstração para coluna 1
-  $('#texto-expandido1').toggle();
-  $('#texto-expandido2').toggle();
-
-  $('#col1').hover(function() {
-    $('#texto-pequeno1').toggle("150");
-    $('#texto-expandido1').toggle("150");
-  });
-
-  // Testar coluna 2
-  $('#col2').hover(function() {
-    $('#texto-pequeno2').toggle("250");
-    $('#texto-expandido2').toggle("250");
-  });
-
+    $('.col').hover(function() {
+        $(this).find('.texto-pequeno').toggle("50");
+        $(this).find('.texto-expandido').toggle("50");
+    });
 });
