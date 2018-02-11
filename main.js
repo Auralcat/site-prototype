15caefbc0ed7da0bcf9fe413b5fed81aff6ddaef
Auralcat <ashleyzt@hi2.in>
2018-02-11 17:03:00 -0200

    Arrumar tamanho da coluna expandida com JS

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
-    // Esconder o texto maior ao carregar página
+  // Achar altura máxima da div
+  var alturaMaxima = 0;
+  $('.col').each(function() {
+    if ($(this).find('.texto-expandido').innerHeight() > alturaMaxima) {
+      alturaMaxima = $(this).find('.texto-expandido').innerHeight();
+    }
+  });
+
+  // Aplicar na classe coluna com um pouco de folga pro rodapé não mexer
+  $('.col').css('min-height', (alturaMaxima * 1.2) + 'px');
+
+  // Esconder o texto maior ao carregar página
     $('.texto-expandido').hide();
 
-    $('.col').hover(function() {
-        $(this).find('.texto-pequeno').toggle("50");
-        $(this).find('.texto-expandido').toggle("50");
+    $('.col').click(function() {
+      alert("Altura: "+ $('.col').height() + "px");
+      $(this).find('.texto-pequeno').toggle("50");
+      $(this).find('.texto-expandido').toggle("50");
     });
+
 });
